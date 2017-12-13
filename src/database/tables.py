1a3ffd0ecee60f02960ffe22562bb5c6fec2182f
Vasily9 <lvn0509@mail.ru>
2017-12-13 16:15:19 +0300

    add files

--- src/database/tables.py
+++ src/database/tables.py
@@ -1,10 +1,10 @@
-# Для работа с базой данных
+# Р”Р»СЏ СЂР°Р±РѕС‚Р° СЃ Р±Р°Р·РѕР№ РґР°РЅРЅС‹С…
 from pony.orm import *
 
 l_db = Database()
 u_db = Database()
 
-#Книга в базе данных
+#РљРЅРёРіР° РІ Р±Р°Р·Рµ РґР°РЅРЅС‹С…
 class Book(l_db.Entity):
     id = PrimaryKey(int)
     title = Required(str, 256)
@@ -12,13 +12,13 @@ class Book(l_db.Entity):
     lang = Optional(str, 2)
     file_type = Optional(str, 4)
 
-#Номер книги в базе данных
+#РќРѕРјРµСЂ РєРЅРёРіРё РІ Р±Р°Р·Рµ РґР°РЅРЅС‹С…
 class FileId(l_db.Entity):
     book_id = Required(int)
     file_type = Required(str, 4)
     file_id = PrimaryKey(str, 64)
 
-#Автор	
+#РђРІС‚РѕСЂ	
 class Author(l_db.Entity):
     id = PrimaryKey(int)
     first_name = Optional(str, 99)
@@ -26,7 +26,7 @@ class Author(l_db.Entity):
     middle_name = Optional(str, 99)
     books = Set(Book, reverse='authors')
 
-	#Полное имя автора
+	#РџРѕР»РЅРѕРµ РёРјСЏ Р°РІС‚РѕСЂР°
     @property
     def normal_name(self) -> str:
         temp = ''
@@ -43,7 +43,7 @@ class Author(l_db.Entity):
         if temp:
             return temp
 
-	#Сокращенное имя автора		
+	#РЎРѕРєСЂР°С‰РµРЅРЅРѕРµ РёРјСЏ Р°РІС‚РѕСЂР°		
     @property
     def short(self) -> str:
         temp = ''
@@ -59,12 +59,12 @@ class Author(l_db.Entity):
             temp += self.middle_name[0]
         return temp
 
-	#Отправка имени	
+	#РћС‚РїСЂР°РІРєР° РёРјРµРЅРё	
     @property
     def to_send(self) -> str:
-        return f'рџ‘¤ <b>{self.normal_name}</b>\n/a_{self.id}\n\n'
+        return f'СЂСџвЂВ¤ <b>{self.normal_name}</b>\n/a_{self.id}\n\n'
 
-#Пользователь
+#РџРѕР»СЊР·РѕРІР°С‚РµР»СЊ
 class User(u_db.Entity):
     id = PrimaryKey(int)
     allow_be = Required(bool, default=True)
